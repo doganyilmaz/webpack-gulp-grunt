36c0c6fe51999832a5e369134099953a6b34c172
doganyilmaz <dy@doganyilmaz.com>
2022-03-12 17:49:21 +0300

    j

--- webpack-3/yedek.js
+++ webpack-3/yedek.js
@@ -7,7 +7,7 @@ module.exports = [{
             path: path.resolve(__dirname, 'wwwroot/assets/js'),
             filename: 'main.js'
         },
-
+        watch: true
     },
 
     //TYPESCRIPT
@@ -25,9 +25,11 @@ module.exports = [{
             extensions: ['.tsx', '.ts', '.js'],
         },
         output: {
+            library: 'app',
             filename: 'test-ts.js',
             path: path.resolve(__dirname, './wwwroot/assets/js'),
         },
+        watch: true
     },
 
     //SASS
@@ -62,9 +64,48 @@ module.exports = [{
             extensions: ['.scss'],
         },
         output: {
-            filename: 'sass.css',
+            filename: './../../../dist/sass-map.css',
             path: path.resolve(__dirname, 'wwwroot/assets/css'),
-        }
+        },
+        watch: true
+    },
+
+    // LESS
+    {
+        mode: 'production',
+        entry: './src/less/less.less',
+        module: {
+            rules: [{
+
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [{
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: 'wwwroot/assets/css',
+                            name: '[name].min.css'
+                        }
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            sourceMap: true,
+                            lessOptions: {
+                                strictMath: true
+                            },
+                        },
+                    },
+                ]
+            }]
+        },
+        resolve: {
+            extensions: ['.less'],
+        },
+        output: {
+            filename: './../../../dist/less-map.css',
+            path: path.resolve(__dirname, 'wwwroot/assets/css'),
+        },
+        watch: true
     },
 
 
